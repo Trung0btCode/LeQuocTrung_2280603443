4a43d1a12e78300287d414b9767f09c97c7fe3a5
Trung0btCode <cutrung2k4@gmail.com>
2025-03-01 17:26:29 +0700

    [update] lab-01 ex03

--- Lab_01/ex03/ex03_06.py
+++ Lab_01/ex03/ex03_06.py
@@ -4,10 +4,10 @@ def xoa_phan_tu(dictionary, key):
         return True
     else:
         return False
-my_dict = eval(input("Nhập dictionary, ví dụ {'a': 1, 'b': 2}: "))
+my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
 key_to_delete = 'b'
 result = xoa_phan_tu(my_dict, key_to_delete)
 if result:
     print("Phần tử đã được xóa khỏi dictionary", my_dict)
-else:dd
-    print("Không tìm thấy phần tử cần xóa trong dictionary")
+else:
+    print("Không tìm thấy phần tử cần xóa trong dictionary.")
